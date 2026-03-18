267346622b162ee0a63ad9bcb05b99fcab43964a
retospeerli <reto.speerli@gmail.com>
2026-03-18 09:16:22 +0100

    Create game.js

---
+++ js/game.js
@@ -0,0 +1,25 @@
+// Spiellogik
+const game = {
+  score: 0,
+  animals: [],
+  currentQuestion: null,
+
+  generateQuestion() {
+    // Code zum Generieren einer zufälligen Mathe-Aufgabe
+  },
+
+  checkAnswer(userAnswer) {
+    // Code zum Überprüfen der Antwort und Aktualisieren des Spielstands
+  },
+
+  buyAnimal(animal) {
+    // Code zum "Kaufen" eines Tieres und Hinzufügen zur Koppel
+  },
+
+  start() {
+    // Code zum Starten des Spiels
+  }
+};
+
+// Initialisierung des Spiels
+game.start();
