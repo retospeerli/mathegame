f210a566f5e9d07464b53f57772c3792e22d555c
retospeerli <reto.speerli@gmail.com>
2026-03-18 09:19:27 +0100

    Create util.js

---
+++ js/util.js
@@ -0,0 +1,12 @@
+// Hilfsfunktionen
+function createButton(text, onClick) {
+  // Code zum Erstellen eines Antwort-Buttons
+}
+
+function updateScore(score) {
+  // Code zum Aktualisieren der Punktzahl-Anzeige
+}
+
+function addAnimalToFarm(animal) {
+  // Code zum Hinzufügen eines Tieres zur Koppel-Anzeige
+}
